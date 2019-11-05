04b977fac519a957e3017a6a1afda1b03c953180
Letícia <leticia.sz.sr@gmail.com>
2019-11-05 17:44:13 -0300

    create file .c / update readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Concorrente_TrabFinal
-Trabalho final de Programação Concorrente
+
+## Trabalho final de Programação Concorrente *2019-2*
+
+Problema do Banheiro Unissex
+
+* **Compile_execute:** gcc -pthread banheiro_unissex.c -o exec && ./exec && rm exec
---
+++ banheiro_unissex.c
@@ -0,0 +1,15 @@
+// autor: Arthur da Veiga Feitoza Borges
+//        Leticia de Souza Soares
+// arquivo: banheiro_unissex.c
+// Trabalho de Prog. Concorrente 2019-2
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+int main()
+{
+    return 0;
+}
