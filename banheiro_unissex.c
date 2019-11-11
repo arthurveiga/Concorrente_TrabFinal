110cb5856956d40f718eeb3cd7fe81a4e3190194
Letícia <leticia.sz.sr@gmail.com>
2019-11-11 10:23:49 -0300

    print organizado

--- banheiro_unissex.c
+++ banheiro_unissex.c
@@ -52,11 +52,11 @@ void input_test()
 int time_banheiro(int genero, int posicao_fila, int alivio)
 {
 
-    int tempo = 1 + rand() % 3; // pra testes um valor pequeno
+    int tempo = 1 + rand() % 10; // pra testes um valor pequeno
     sleep(tempo);
-    printf("------------Tempo-------------------\n");
+    printf("------------ Tempo------------------\n");
     print_genero(genero, posicao_fila);
-    printf("Utilizando banheiro: %d tempos\n", tempo);
+    printf("Utilizando banheiro por %d segundos\n", tempo);
     printf("------------------------------------\n\n");
     alivio = 1;
     return alivio;
@@ -75,7 +75,7 @@ void banheiro_ocupado_mulher(int genero_pessoa, int posicao_fila)
         pthread_mutex_lock(&mutx_entrada_no_banheiro);
         sem_wait(&sem_compartimentos);                               //decrementa compartimentos, aguarda ou bloqueia
         sem_getvalue(&sem_compartimentos, &num_pessoas_no_banheiro); // pega numero de passoas
-        printf("\n Compartimentos = %d\n \n", num_pessoas_no_banheiro);
+        printf("\nCompartimento ocupado pela mulher: %d\nCompartimentos disponíveis = %d\n\n",posicao_fila, num_pessoas_no_banheiro);
         pthread_mutex_unlock(&mutx_entrada_no_banheiro);
     }
     else
@@ -85,7 +85,7 @@ void banheiro_ocupado_mulher(int genero_pessoa, int posicao_fila)
             pthread_mutex_lock(&mutx_entrada_no_banheiro);
             sem_wait(&sem_compartimentos);
             sem_getvalue(&sem_compartimentos, &num_pessoas_no_banheiro);
-            printf("\n Compartimentos = %d\n \n", num_pessoas_no_banheiro);
+            printf("\nCompartimento ocupado pela mulher: %d\nCompartimentos disponíveis = %d\n\n", posicao_fila, num_pessoas_no_banheiro);
             pthread_mutex_unlock(&mutx_entrada_no_banheiro);
         }
         else
@@ -115,7 +115,7 @@ void banheiro_ocupado_homem(int genero_pessoa, int posicao_fila)
         pthread_mutex_lock(&mutx_entrada_no_banheiro);
         sem_wait(&sem_compartimentos);                               //decrementa compartimentos, aguarda ou bloqueia
         sem_getvalue(&sem_compartimentos, &num_pessoas_no_banheiro); // pega numero de passoas
-        printf("\n Compartimentos = %d\n \n", num_pessoas_no_banheiro);
+        printf("\nCompartimento ocupado pelo homem: %d\nCompartimentos disponíveis = %d\n\n", posicao_fila, num_pessoas_no_banheiro);
         pthread_mutex_unlock(&mutx_entrada_no_banheiro);
     }
     else
@@ -125,7 +125,7 @@ void banheiro_ocupado_homem(int genero_pessoa, int posicao_fila)
             pthread_mutex_lock(&mutx_entrada_no_banheiro);
             sem_wait(&sem_compartimentos);
             sem_getvalue(&sem_compartimentos, &num_pessoas_no_banheiro);
-            printf("\n Compartimentos = %d\n \n", num_pessoas_no_banheiro);
+            printf("\nCompartimento ocupado pelo homem: %d\nCompartimentos disponíveis = %d\n\n", posicao_fila, num_pessoas_no_banheiro);
             pthread_mutex_unlock(&mutx_entrada_no_banheiro);
         }
         else
@@ -147,7 +147,12 @@ void banheiro_livre(int genero_pessoa, int posicao_fila)
 {
     sem_post(&sem_compartimentos);                               // libera compartimento
     sem_getvalue(&sem_compartimentos, &num_pessoas_no_banheiro); // pega numero de pessoas
-    printf("Liberando: Compartimentos = %d\n", num_pessoas_no_banheiro);
+
+    if(genero_pessoa == HOMEM)
+        printf("Homem %d saiu do banheiro\nCompartimentos = %d\n", posicao_fila, num_pessoas_no_banheiro);
+    else if (genero_pessoa == MULHER)
+        printf("Mulher %d saiu do banheiro\nCompartimentos = %d\n", posicao_fila, num_pessoas_no_banheiro);
+
     if (num_pessoas_no_banheiro == num_compartimentos) // se atingiu capacidade maxima muda o genero
     {
         pthread_mutex_lock(&mutx_genero_no_banheiro);
